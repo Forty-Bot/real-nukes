128dba2fcabcb2e80e1014032d4321c4acb7a938
Sean Anderson <seanga2@gmail.com>
2018-09-15 22:23:28 -0400

    Add custom graphics

--- prototypes/nuke.lua
+++ prototypes/nuke.lua
@@ -1,8 +1,9 @@
-local projectile = table.deepcopy(data.raw.projectile["atomic-rocket"])
+local projectile = table.deepcopy(data.raw["artillery-projectile"]["artillery-projectile"])
 projectile.name = "nuke-shell"
-projectile.type = "artillery-projectile"
-projectile.reveal_map = true
 projectile.map_color = {r=0, g=1, b=0}
+projectile.picture.filename = "__real-nukes__/graphics/nuke-shell.png"
+projectile.chart_picture.filename = "__real-nukes__/graphics/nuke-shoot-map-visualization.png"
+projectile.action.action_delivery.target_effects = data.raw.projectile["atomic-rocket"].action.action_delivery.target_effects
 
 local nuke = table.deepcopy(data.raw.ammo["artillery-shell"])
 nuke.name = "nuke"
